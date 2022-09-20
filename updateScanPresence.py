e89affe3ed11f7cd8c90cb4885343f3f028554b5
dwwatk02 <87448112+dwwatk02@users.noreply.github.com>
2022-09-20 13:03:19 -0400

    Add files via upload

--- updateScanPresence.py
+++ updateScanPresence.py
@@ -8,8 +8,8 @@ import time
 urllib3.disable_warnings()
 
 #API Key
-keyId="1b1e4de3-b796-bb9a-fa1f-0ef657ae02df"
-keySecret="m4UEZUywdAu8Qh5Sw2ad11ptDAn6JtGnTZqaQ1l57v8="
+keyId=""
+keySecret=""
 
 asoc = ASoC(keyId, keySecret)
 
