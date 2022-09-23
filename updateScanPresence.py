a2ff6daf9ec0413e3c0c7319c4c65c068c70d45e
dwwatk02 <87448112+dwwatk02@users.noreply.github.com>
2022-09-23 17:37:32 -0400

    Add files via upload

--- updateScanPresence.py
+++ updateScanPresence.py
@@ -17,13 +17,15 @@ code, result = asoc.login()
 if code != 200:
 	print(f'error logging into ASOC!! code is {code}')
 
+active_presence = sys.argv[1]
 
-starttime = time.time()
-print(f'--updateScanPresence.py started: {starttime}--')
+## uncomment the following two lines and comment the preceding line to use the first V2 presence ID fetched via API instead of the user-inputted presence ID
+#active_presence_dict = asoc.getActivePresences()
+#active_presence = active_presence_dict[0]['Id']
 
-active_presence = asoc.getActivePresences()
 scanids = asoc.getV1PresenceScans()
-for(scans in scanids):
-	asoc.updateScanPresence(scan_id,active_presence)
-endtime = time.time()
-print(f'--updateScanPresence.py ended: {endtime}--')
+
+for scans in scanids:
+	scan = scans['Id']
+	asoc.updateScanPresence(scans,active_presence)
+
